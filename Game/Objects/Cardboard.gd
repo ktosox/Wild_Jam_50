8e62e899966e6ab0027e810dba869e8e5290a116
Ktosox <ktoso3@gmail.com>
2022-10-16 15:52:47 +0200

    Wrote down a bunch of fluff

--- Game/Objects/Cardboard.gd
+++ Game/Objects/Cardboard.gd
@@ -2,7 +2,9 @@ extends VBoxContainer
 
 class_name Cardboard
 
-var mouse_offset = Vector2.ZERO #offset between mouse and this scene when mouse dragging happens
+var instanceData : ObjectData
+
+var mouseOffset = Vector2.ZERO #offset between mouse and this scene when mouse dragging happens
 
 func _ready():
 	set_physics_process(false)
@@ -13,6 +15,17 @@ func _ready():
 	$Divide.points[1] = Vector2(rect_size.x,0)
 	
 
+func load_object(data : ObjectData):
+	instanceData = data
+	$Image.texture = data.image
+	$Bottom/Layout/Name.text = data.name
+	if data.type == ObjectData.types.Token:
+		$Bottom.visible = false
+		$Border.visible = false
+		$Divide.visible = false
+	
+	pass
+
 func _gui_input(event):
 	if event.is_action_pressed("mouse_click"):
 		grab()
@@ -20,14 +33,14 @@ func _gui_input(event):
 		drop()
 
 func _physics_process(delta):
-	var desired_loc = get_global_mouse_position() - mouse_offset
+	var desired_loc = get_global_mouse_position() - mouseOffset
 	rect_global_position = rect_global_position.move_toward(desired_loc,0.12*rect_global_position.distance_squared_to(desired_loc)*delta)
 	#check if nodes below can take object data
 	pass
 
 func grab():
 	get_parent().move_child(self,get_parent().get_child_count())
-	mouse_offset = get_global_mouse_position() - rect_global_position
+	mouseOffset = get_global_mouse_position() - rect_global_position
 	set_physics_process(true)
 
 
@@ -35,11 +48,12 @@ func drop():
 	set_physics_process(false)
 
 func can_accept_drop(drop_data):
-	# check drop_data and return true is can accept
+	# check drop_data and return true if can accept
 	return false
 
 func accept_drop(drop_data):
 	assert( can_accept_drop(drop_data) )
+	#do stuff after accepting drop data
 	pass
 
 
---
+++ Game/Objects/object_data.gd
@@ -0,0 +1,11 @@
+extends Resource
+
+class_name ObjectData
+
+enum types {Item, Place, Power, Situation, Token}
+
+export (types) var type
+
+export var name : String
+
+export var image : Texture
